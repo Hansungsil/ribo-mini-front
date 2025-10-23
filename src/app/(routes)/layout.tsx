653c981d4d25fb76b30f18544d075077957aca6c
sungsil <han_148@naver.com>
2025-10-23 23:25:43 +0900

    feat: [4] appla style dock 추가

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,12 +1,14 @@
 import Header from "./_components/Header";
 import Nav from "./_components/Nav";
 import Footer from "./_components/Footer";
+import AppleStyleDock from "./_components/AppleStyleDock";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return <>
     <Header />
     <Nav />
     {children}
+    <AppleStyleDock />
     <Footer />
   </>;
 };
