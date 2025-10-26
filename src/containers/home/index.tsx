f2306591ad756be183666a6771b8015376af7452
sungsil <han_148@naver.com>
2025-10-26 23:40:58 +0900

    feat: [2] home - wavy background 적용

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,7 +1,31 @@
+import { WavyBackground } from "@/components/ui/shadcn-io/wavy-background";
+import cx from "classnames";
+import styles from "./HomeContainer.module.scss";
+
+
 const HomeContainer = () => {
     return (
-        <div>
-            <h1>Home!!!!!!!!!!!!!!!</h1>
+        <div className="relative h-screen w-full overflow-hidden">
+            <WavyBackground
+                backgroundFill="#0f0f23"
+                colors={["#38bdf8", "#818cf8", "#c084fc", "#e879f9"]}
+                waveWidth={50}
+                blur={10}
+                speed="fast"
+                waveOpacity={0.5}
+                containerClassName="h-full w-full"
+                className="flex items-center justify-center"
+            >
+                {/* Your content flows above */}
+                <div className="text-center text-white z-10">
+                    <h1 className={cx("text-6xl font-bold mb-4", styles.fontSairaBold)}>
+                        🎉Ribo Mini Homepage✨
+                    </h1>
+                    <p className={cx("text-xl opacity-80", styles.fontSairaExtraLight)}>
+                        this very simple page is created by xoxhaan
+                    </p>
+                </div>
+            </WavyBackground>
         </div>
     );
 };
